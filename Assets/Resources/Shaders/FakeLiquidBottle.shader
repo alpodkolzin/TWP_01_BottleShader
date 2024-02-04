8e224ec67a780645e362eaab4114a67883731879
alpodkolzin <al.podkolzin.0@gmail.com>
2024-02-04 17:05:43 +0400

    Add wobble implementation by MinionsArt

--- Assets/Resources/Shaders/FakeLiquidBottle.shader
+++ Assets/Resources/Shaders/FakeLiquidBottle.shader
@@ -3,6 +3,7 @@ Shader "Unlit/FakeLiquid"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
+
         [PerRendererData] _FillAmount("FillAmount", Vector) = (0,0,0,0) 
     }
     SubShader
@@ -33,7 +34,27 @@ Shader "Unlit/FakeLiquid"
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
+
             float3 _FillAmount;
+            float _WobbleX, _WobbleZ;
+
+            // https://docs.unity3d.com/Packages/com.unity.shadergraph@6.9/manual/Rotate-About-Axis-Node.html
+            float3 Unity_RotateAboutAxis_Degrees(float3 In, float3 Axis, float Rotation)
+            {
+                Rotation = radians(Rotation);
+                float s = sin(Rotation);
+                float c = cos(Rotation);
+                float one_minus_c = 1.0 - c;
+
+                Axis = normalize(Axis);
+                float3x3 rot_mat = 
+                {   one_minus_c * Axis.x * Axis.x + c, one_minus_c * Axis.x * Axis.y - Axis.z * s, one_minus_c * Axis.z * Axis.x + Axis.y * s,
+                    one_minus_c * Axis.x * Axis.y + Axis.z * s, one_minus_c * Axis.y * Axis.y + c, one_minus_c * Axis.y * Axis.z - Axis.x * s,
+                    one_minus_c * Axis.z * Axis.x - Axis.y * s, one_minus_c * Axis.y * Axis.z + Axis.x * s, one_minus_c * Axis.z * Axis.z + c
+                };
+                float3 Out = mul(rot_mat,  In);
+                return Out;
+            }
 
             v2f vert (appdata v)
             {
@@ -45,7 +66,13 @@ Shader "Unlit/FakeLiquid"
                 // but we don't need to apply world transformation, so we not using 'w'
                 float3 worldPos = mul(unity_ObjectToWorld, float4(v.vertex.xyz, 0));
 
-                o.fillPosition =  _FillAmount - worldPos;
+                // rotate it around XY and XZ
+                float3 worldPosX= Unity_RotateAboutAxis_Degrees(worldPos, float3(0,0,1),90);
+                float3 worldPosZ = Unity_RotateAboutAxis_Degrees(worldPos, float3(1,0,0),90);
+                // combine rotations with worldPos, based on sine wave from script
+                float3 worldPosAdjusted = worldPos + worldPosX  * _WobbleX + worldPosZ * _WobbleZ;
+
+                o.fillPosition = _FillAmount - worldPosAdjusted;
 
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
                 return o;
--- Assets/Scripts/FakeLiquidBottle.cs
+++ Assets/Scripts/FakeLiquidBottle.cs
@@ -7,12 +7,103 @@ public class FakeLiquidBottle : MonoBehaviour
     [SerializeField] private MeshRenderer m_meshRenderer;
     [SerializeField] private Collider m_collider;
 
+    [Space]
+    [SerializeField] float MaxWobble = 0.03f;
+    [SerializeField] float WobbleSpeedMove = 1f;
+    [SerializeField] float Recovery = 1f;
+    [SerializeField] float Thickness = 1f;
+
+
     private static readonly int FillAmount = Shader.PropertyToID("_FillAmount");
+
+    private float time;
+    private float pulse;
+    private float sinewave;
+    float wobbleAmountX;
+    float wobbleAmountZ;
+    private float wobbleAmountToAddX;
+    private float wobbleAmountToAddZ;
+    private Vector3 velocity;
+    private Vector3 angularVelocity;
+    private Vector3 lastPos;
+    private Quaternion lastRot;
 
     // For convenience let' leave the Update method
     private void Update()
     {
         UpdateFillPosition();
+        UpdateWobble();
+    }
+
+    private void UpdateWobble()
+    {
+        float deltaTime = Time.deltaTime;
+        time += Time.deltaTime;
+
+        // decrease wobble over time
+        wobbleAmountToAddX = Mathf.Lerp(wobbleAmountToAddX, 0, deltaTime * Recovery);
+        wobbleAmountToAddZ = Mathf.Lerp(wobbleAmountToAddZ, 0, deltaTime * Recovery);
+
+        // make a sine wave of the decreasing wobble
+        pulse = 2 * Mathf.PI * WobbleSpeedMove;
+        sinewave = Mathf.Lerp(sinewave, Mathf.Sin(pulse * time),
+            deltaTime * Mathf.Clamp(velocity.magnitude + angularVelocity.magnitude, Thickness, 10));
+
+        wobbleAmountX = wobbleAmountToAddX * sinewave;
+        wobbleAmountZ = wobbleAmountToAddZ * sinewave;
+
+        // velocity
+        velocity = (lastPos - m_meshRenderer.transform.position) / deltaTime;
+
+        angularVelocity = GetAngularVelocity(lastRot, m_meshRenderer.transform.rotation);
+
+        // add clamped velocity to wobble
+        wobbleAmountToAddX +=
+            Mathf.Clamp((velocity.x + (velocity.y * 0.2f) + angularVelocity.z + angularVelocity.y) * MaxWobble,
+                -MaxWobble, MaxWobble);
+        wobbleAmountToAddZ +=
+            Mathf.Clamp((velocity.z + (velocity.y * 0.2f) + angularVelocity.x + angularVelocity.y) * MaxWobble,
+                -MaxWobble, MaxWobble);
+
+        // send it to the shader
+        m_meshRenderer.material.SetFloat("_WobbleX", wobbleAmountX);
+        m_meshRenderer.material.SetFloat("_WobbleZ", wobbleAmountZ);
+
+        // keep last position
+        lastPos = m_meshRenderer.transform.position;
+        lastRot = m_meshRenderer.transform.rotation;
+    }
+    
+    //https://forum.unity.com/threads/manually-calculate-angular-velocity-of-gameobject.289462/#post-4302796
+    Vector3 GetAngularVelocity(Quaternion foreLastFrameRotation, Quaternion lastFrameRotation)
+    {
+        var q = lastFrameRotation * Quaternion.Inverse(foreLastFrameRotation);
+        // no rotation?
+        // You may want to increase this closer to 1 if you want to handle very small rotations.
+        // Beware, if it is too close to one your answer will be Nan
+        if (Mathf.Abs(q.w) > 1023.5f / 1024.0f)
+            return Vector3.zero;
+        float gain;
+        // handle negatives, we could just flip it but this is faster
+        if (q.w < 0.0f)
+        {
+            var angle = Mathf.Acos(-q.w);
+            gain = -2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+        else
+        {
+            var angle = Mathf.Acos(q.w);
+            gain = 2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+
+        Vector3 angularVelocity = new Vector3(q.x * gain, q.y * gain, q.z * gain);
+
+        if (float.IsNaN(angularVelocity.z))
+        {
+            angularVelocity = Vector3.zero;
+        }
+
+        return angularVelocity;
     }
 
     private void UpdateFillPosition()
@@ -27,7 +118,6 @@ public class FakeLiquidBottle : MonoBehaviour
         // which itself is distance from the bounds point to the point of liquid level (only Y coordinate)
         Vector3 fillPos = bounds.center - distanceFromBoundsCenter - m_meshRenderer.transform.position;
 
-        // m_meshRenderer.sharedMaterial.SetVector(FillAmount, fillPos);
         MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
         materialPropertyBlock.SetVector(FillAmount, fillPos);
         m_meshRenderer.SetPropertyBlock(materialPropertyBlock);
